e1481902003bf8031e8291dd3ddd981a0f5149cf
Ayoub-Vip <Vous@exemple.com>
2022-05-11 23:11:13 +0200

    pas tres important

--- admin/mytools/functions/projet.php
+++ admin/mytools/functions/projet.php
@@ -34,10 +34,13 @@ if($_POST['display_PROJET']){
     if($check){
         $req = $bdd->query("SELECT * FROM PROJET WHERE NOM LIKE '%".$NOM."%' OR DEPARTEMENT LIKE '%".$DEPARTEMENT."%' OR 
             CHEF LIKE '%".$CHEF."%' OR BUDGET = '".$BUDGET."' OR DATE_DEBUT = '".$DATE_DEBUT."' OR COUT = '".$COUT."' OR DATE_FIN ='".$DATE_FIN."'  ");
+
+
+
         echo "<h5>INSERT INTO  PROJET VALUES  NOM LIKE '%".$NOM."%' OR DEPARTEMENT LIKE '%".$DEPARTEMENT."%' OR 
             CHEF LIKE '%".$CHEF."%' OR BUDGET = '".$BUDGET."' OR DATE_DEBUT = '".$DATE_DEBUT."' OR COUT = '".$COUT."' OR DATE_FIN ='".$DATE_FIN."' </h5> ";
 
-            
+
         echo '<h2>list of projects in database</h2>';
         echo "<table class=\"datatable\">
         <tr><th>NOM</th><th>DEPARTEMENT</th><th>DATE_DEBUT</th><th>CHEF</th><th>BUDGET</th><th>COUT</th><th>DATE_FIN</th></tr>";
@@ -54,7 +57,7 @@ if($_POST['display_PROJET']){
 
 
 
-<h1>FETCH DATA</h1>
+<h1>INSERT DATA</h1>
 
 <form action="<?PHP echo $PHP_SELF; ?>" method="post">
     <input id="search" placeholder="NOM" type="text" name="NOM">
