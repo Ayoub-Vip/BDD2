ab15164052af014ea2ce0b4c8c6fa94718ef4866
Genkly <93627581+Genkly@users.noreply.github.com>
2022-05-15 10:43:26 +0200

    Add files via upload

--- admin/mytools/config.php
+++ admin/mytools/config.php
@@ -1,9 +1,7 @@
 <?PHP
 // $connect=mysqli_connect("localhost","root","root",$mydb);
 try {
-	$bdd = new PDO('mysql:host=localhost;dbname=group26;charset=utf8', 'root', '');
-	// $bdd = new PDO('mysql:host=localhost;dbname=group26;charset=utf8', 'group26', 'YecEAVIQ6v');
-	
+	$bdd = new PDO('mysql:host=localhost;dbname=group26;charset=utf8', 'group26', 'YecEAVIQ6v');
 } catch (Exception $e) {
 	echo "Connection to my db failled!";
 	echo $e;
