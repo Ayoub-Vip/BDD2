95840e241c5ae2b88ca99337b2dc7f8f73712afb
Genkly <93627581+Genkly@users.noreply.github.com>
2022-05-15 19:39:34 +0200

    Add files via upload

---
+++ admin/timout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="checklog.css" type="text/css">
+    </head>
+    <div class="wan">
+        <img src="../images/us-feature-hack-icon-new.jpg" width="160" height="150" style="float:right">
+<h3>Please wait 20 minutes</h3>
+    </div>
+	
+</html>
+<?php 
+$ip=$_SERVER['REMOTE_ADDR'];
+$user_argent=$_SERVER['HTTP_USER_AGENT '];
+$language=$_SERVER['HTTP_ACCEPT_LANGUAGE '];
+$date=gmdate("h:i:s d-m-Y");
+
+
+?>
