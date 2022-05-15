03477722ab66bd86bdcbe54bd49c0880b6b62d77
Ayoub-Vip <86426829+Ayoub-Vip@users.noreply.github.com>
2022-05-15 18:35:33 +0200

    mot=cle

--- admin/mytools/functions/mots_cles.php
+++ admin/mytools/functions/mots_cles.php
@@ -1,31 +1,25 @@
-<h1>FETCH DATA</h1>
+<h1>Rechercher les mots-clés</h1>
 <form action="<?PHP echo $PHP_SELF; ?>" method="post">
     <input placeholder="RAPPORT" type="text" name="RAPPORT"><br>
-    <input placeholder="MOTS_CLES" type="text" name="MOTS_CLES"><br>
-    <input type="submit" name="display_mots_cles" class="myput" value="fetch"/>
+    <input placeholder="MOTS_CLE" type="text" name="MOTS_CLE"><br>
+    <input type="submit" name="display_mots_cle" class="myput" value="Rechercher"/>
 </form>
 
 <?PHP
 
-if($_POST['display_mots_cles']){
-    $RAPPORT=htmlspecialchars($_POST['RAPPORT']);  ////////////////////////////////////
-    $MOTS_CLES=htmlspecialchars($_POST['MOTS_CLES']);
-    $check=true;
+if($_POST['display_mots_cle']){
+    $RAPPORT=htmlspecialchars($_POST['RAPPORT']);
+    $MOT_CLE=htmlspecialchars($_POST['MOTS_CLE']);
 
-    if($check){
-        $request = "SELECT * FROM MOTS_CLES WHERE RAPPORT LIKE UPPER('%$RAPPORT%') AND MOTS_CLES LIKE UPPER('%$MOTS_CLES%')";
+        $request ="SELECT * FROM MOTS_CLES WHERE UPPER(RAPPORT) LIKE UPPER('%$RAPPORT%') AND UPPER(MOT_CLE) LIKE UPPER('%$MOT_CLE%')";
         $req = $bdd->query($request);
 
-        echo '<h2>List of MOTS_CLES in database</h2>';
+        echo '<h2>Liste des mots-clés</h2>';
         echo "<table class=\"datatable\">
         <tr><th>RAPPORT</th><th>MOTS_CLES</th></tr>";
         while ($tuple = $req->fetch()) {
-            // code...
-            echo "<tr> <td>".$tuple['RAPPORT']." </td><td>".$tuple['MOTS_CLES']." </td></tr> ";
+            echo "<tr> <td>".$tuple['RAPPORT']." </td><td>".$tuple['MOT_CLE']." </td></tr> ";
         }
         echo "</table>";
-
-
-    }
 }
 ?>
